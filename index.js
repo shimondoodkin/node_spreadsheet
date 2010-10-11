ef4308092fc6899ac627dedb353d4351d0718e6f
Shimon Doodkin <helpmepro1@gmail.com>
2010-10-11 03:32:37 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -112,8 +112,8 @@ function read(inputfile,callback,options)
       if (err) throw err;
       //if (err2) throw err;
       //console.log(data.toString());
-      callback(JSON.parse('('+data+')'));
-      //callback(eval(data));
+      //callback(JSON.parse('('+data+')'));
+      callback(eval(data));
      });
     });
    }
@@ -123,4 +123,4 @@ function read(inputfile,callback,options)
 } this.read=read;
 
 //test:
-read(__dirname+"/Book1.xls",function(obj){console.log(obj);});
+//read(__dirname+"/Book1.xls",function(obj){console.log(obj);});
